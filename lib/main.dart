280e00cc95a42774b6c022b8a369c2e06b458924
abukamel98 <mohammadamleh1998@gmail.com>
2023-08-05 23:16:48 +0300

    task3

--- lib/main.dart
+++ lib/main.dart
@@ -1,254 +1,148 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(Homepage());
+  runApp(const MyApp());
 }
 
-class Homepage extends StatefulWidget {
-  const Homepage({super.key});
-
-  @override
-  State<Homepage> createState() => _HomepageState();
-}
-
-class _HomepageState extends State<Homepage> {
-  int mainindex = 0;
-  int corrects = 0;
-  int numberofQuestion = 0;
-  bool errormassege = false;
-  int? yourChoiceindex;
-  bool quizappfinished = false;
-  bool isAnswer = false;
-  List questions = [
-    "what if my favorite food",
-    'what is my favorite color',
-    'what is my job ',
-  ];
-  List Anwsers = [
-    ["mensaf", "burger", 'pizza'],
-    ['blue', 'red', 'black'],
-    ['doctor', 'worker', 'Engineer'],
-  ];
-  List AnwsersIcons = [
-    [
-      "https://upload.wikimedia.org/wikipedia/commons/2/2c/%D8%B3%D8%AF%D8%B1_%D9%85%D9%86%D8%B3%D9%81_02.jpg",
-      "https://img.freepik.com/free-photo/front-view-tasty-meat-burger-with-cheese-salad-dark-background_140725-89597.jpg",
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/8/86/Pizza_%281%29.jpg/1200px-Pizza_%281%29.jpg'
-    ],
-    [
-      'https://upload.wikimedia.org/wikipedia/commons/e/e4/Color-blue.JPG',
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/f/fc/Colour_red.jpg/1200px-Colour_red.jpg',
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/A_black_image.jpg/1200px-A_black_image.jpg'
-    ],
-    [
-      'https://img.freepik.com/free-photo/attractive-young-male-nutriologist-lab-coat-smiling-against-white-background_662251-2960.jpg',
-      'https://st2.depositphotos.com/1017986/42985/i/450/depositphotos_429851104-stock-photo-happy-male-worker-or-builder.jpg',
-      'https://st5.depositphotos.com/10539404/64672/i/600/depositphotos_646723908-stock-photo-asian-engineer-handsome-man-architect.jpg'
-    ],
-  ];
-  List correctAnswer = ["mensaf", "blue", 'Engineer'];
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    double heighOffDevice = MediaQuery.of(context).size.height;
-    double widthOffDevice = MediaQuery.of(context).size.width;
     return MaterialApp(
-        theme: ThemeData.light(),
-        home: Scaffold(
-          body: SafeArea(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 14),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  SizedBox(
-                    height: heighOffDevice * 0.15,
-                  ),
-                  if (quizappfinished == true) ...[
-                    Text(
-                      "congratulation you finish the quiz app your score is $corrects /${questions.length}",
-                      style:
-                      TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-                    ),
-                    ElevatedButton(
-                      onPressed: () {
-                        setState(() {
-                          mainindex = 0;
-                          numberofQuestion = 0;
-                          quizappfinished = false;
-                          corrects=0;
-                        });
-                      },
-                      child: Text('try again'),
-                    )
-                  ],
-                  if (quizappfinished == false) ...[
-                    Text(
-                      questions[mainindex],
-                      style:
-                      TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-                    ),
-                    SizedBox(
-                      height: 13,
-                    ),
-                    Text(
-                      "Anwser and get points",
-                      style: TextStyle(fontSize: 16, color: Colors.black54),
-                    ),
-                    SizedBox(
-                      height: heighOffDevice * 0.1,
-                    ),
-                  ],
-                  Column(
-                    children: [
-                      Text("$numberofQuestion /${questions.length}"),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 10),
-                        child: Row(
-                          children: [
-                            Container(
-                              width: 110,
-                              height: 5,
-                              decoration: BoxDecoration(
-                                  border: Border.all(color: Colors.black54),
-                                  color: numberofQuestion > 0
-                                      ? Colors.green
-                                      : Colors.white),
-                            ),
-                            SizedBox(
-                              width: 5,
-                            ),
-                            Container(
-                              width: 110,
-                              height: 5,
-                              decoration: BoxDecoration(
-                                  border: Border.all(color: Colors.black54),
-                                  color: numberofQuestion > 1
-                                      ? Colors.green
-                                      : Colors.white),
-                            ),
-                            SizedBox(
-                              width: 5,
-                            ),
-                            Container(
-                              width: 110,
-                              height: 5,
-                              decoration: BoxDecoration(
-                                  border: Border.all(color: Colors.black54),
-                                  color: numberofQuestion > 2
-                                      ? Colors.green
-                                      : Colors.white),
-                            ),
-                          ],
-                        ),
-                      )
-                    ],
-                  ),
-                  if (quizappfinished == false) ...[
-                    if (errormassege == true)
-                      Text(
-                        "*please choice one option* ",
-                        style: TextStyle(color: Colors.red),
-                      ),
-                    SizedBox(
-                      height: 5,
-                    ),
-                    for (int i = 0; i < Anwsers[mainindex].length; i++) ...[
-                      Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 6),
-                        child: InkWell(
-                          onTap: () {
-                            setState(() {
-                              yourChoiceindex = i;
-                              if (errormassege == true) errormassege = false;
-                              if(correctAnswer[mainindex]==Anwsers[mainindex][i] && !isAnswer) {
-                                corrects++;
-                                isAnswer = true;
-                              }
-                            });
-                          },
-                          child: Container(
-                            height: 60,
-                            decoration: BoxDecoration(
-                                border: Border.all(
-                                    color: Colors.grey.withOpacity(0.5)),
-                                color: i != yourChoiceindex
-                                    ? Colors.white
-                                    : Colors.green,
-                                borderRadius: BorderRadius.circular(12)),
-                            child: Padding(
-                              padding:
-                              const EdgeInsets.symmetric(horizontal: 15),
-                              child: Row(
-                                children: [
-                                  ClipRRect(
-                                      borderRadius: BorderRadius.circular(20),
-                                      child: Container(
-                                        child: Image.network(
-                                          AnwsersIcons[mainindex][i],
-                                          fit: BoxFit.cover,
-                                          width: 50,
-                                          height: 50,
-                                          errorBuilder: (ctx,err,trace){
-                                            return Text("no imternet");
-                                          },
-                                        ),
-                                      )),
-                                  SizedBox(
-                                    width: 15,
-                                  ),
-                                  Text(
-                                    Anwsers[mainindex][i],
-                                    style: TextStyle(
-                                        color: i != yourChoiceindex
-                                            ? Colors.black
-                                            : Colors.white,
-                                        fontSize: 18),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
-                    Spacer(),
-                    Container(
-                      width: double.infinity,
-                      height: 50,
-                      child: ElevatedButton(
-                        onPressed: () {
-                          setState(() {
-                            if (yourChoiceindex != null &&
-                                mainindex <= questions.length - 1) {
-                              mainindex++;
-                              yourChoiceindex = null;
-                              errormassege = false;
-                              numberofQuestion++;
-                              print(mainindex);
-                              print(" q  $numberofQuestion");
-                              isAnswer = false;
-                            } else if (yourChoiceindex == null)
-                              errormassege = true;
-                            if (mainindex > questions.length - 1)
-                              quizappfinished = true;
-                          });
-                        },
-                        child: Text("Next"),
-                        style: ElevatedButton.styleFrom(
-                            backgroundColor: Colors.black,
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(8))),
-                      ),
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                  ]
-                ],
-              ),
-            ),
-          ),
-        ));
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: MyHomePage(),
+    );
   }
 }
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({super.key});
+  List QuestionsAnsers = [
+    {
+      "question": "what is 1+1",
+      "answer": ["1", "2", "3", "4"],
+      "trueAnswer": "2"
+    },
+    {
+      "question": "what is 4+4",
+      "answer": ["5", "6", "8", "7"],
+      "trueAnswer": "8"
+    },
+    {
+      "question": "what is 10+10",
+      "answer": ["15", "20", "11", "12"],
+      "trueAnswer": "20"
+    },
+    {
+      "question": "what is 10+8",
+      "answer": ["18", "26", "38", "47"],
+      "trueAnswer": "18"
+    },
+  ];
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int num = 0;
+
+  int correct = 0;
+
+  bool isLastQuestionPressed = false;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("quizapp"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              widget.QuestionsAnsers[num]["question"],
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            for (int x = 0;
+            x <
+                (widget.QuestionsAnsers[num]["answer"] as List<String>)
+                    .length;
+            x++) ...[
+              MyElevatedButton(
+                answerIndex: x,
+                onPress: (answerIndex) {
+                  setState(() {
+
+                    if(widget.QuestionsAnsers[num]["trueAnswer"] == widget.QuestionsAnsers[num]["answer"][answerIndex] && !isLastQuestionPressed ){
+                      correct++;
+                    }
+
+                    if (num != widget.QuestionsAnsers.length-1) {
+                      num++;
+                    }else  {
+                      isLastQuestionPressed = true;
+                    }
+
+                  });
+                },
+                text: widget.QuestionsAnsers[num]["answer"][x],
+              )
+            ],
+            Container(
+              child: isLastQuestionPressed
+                  ? //check if loading is true or false
+              Column(
+                children: [
+                  Text("correct ${correct}/${widget.QuestionsAnsers.length}"),
+                  ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          num = 0;
+                          correct = 0;
+                          isLastQuestionPressed = false;
+                        });
+                      },
+                      child: Text("reload")),
+                ],
+              )
+                  : //show progress on loading = true
+              Container(), //show this text on loading = false
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class MyElevatedButton extends StatelessWidget {
+  final int answerIndex;
+  final Function(int) onPress;
+  final String text;
+  const MyElevatedButton({
+    super.key,
+    required this.answerIndex,
+    required this.onPress,
+    required this.text,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: (){
+        onPress(answerIndex);
+      },
+      child: Text(text),
+    );
+  }
+}
+
+
